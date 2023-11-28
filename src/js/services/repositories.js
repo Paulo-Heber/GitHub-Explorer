8535102888cece45d61bba501bf63dbe3b271d1d
Paulo Heber <pauloheber2017@gmail.com>
2023-11-28 17:55:55 -0300

    te xte 4

--- src/js/services/repositories.js
+++ src/js/services/repositories.js
@@ -1,4 +1,4 @@
-import { baseUrl, repositoriesQuantity } from "./src/js/variables.js"
+import { baseUrl, repositoriesQuantity } from "/src/js/variables.js"
 
 async function getRepositories(userName) {
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
