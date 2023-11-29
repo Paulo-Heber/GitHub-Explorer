4e0a80d8a01a916759323cd1d736f47a6de51b18
Paulo Heber <pauloheber2017@gmail.com>
2023-11-29 13:16:40 -0300

    modificando os caminhos das importações

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import { getUser } from "./src/js/services/user.js"
-import { getEvents } from "./src/js/services/events.js"
-import { getRepositories } from "./src/js/services/repositories.js"
+import { getEvents } from "./js/services/events.js"
+import { getRepositories } from "./services/repositories.js"
 import { user } from "./src/js/objects/user.js"
 import { screen } from "./src/js/objects/screen.js"
 
